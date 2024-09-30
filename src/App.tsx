975a8fadd08de1bca00b81aa39327df89f90dd4c
Meghan Hickey <mhickey@pryoninc.com>
2024-09-30 12:28:21 -0500

    display colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import colorData from '../public/colors.json';
+
+type Color = {
+    hex: string;
+    name: string;
+    comp: { hex: string; name: string }[];
+};
+
 const App = () => {
-    return <h1>Hello World</h1>;
+    return colorData.map((color: Color) => {
+        return (
+            <div key={color.hex}>
+                <div>{color.name}: {color.hex} </div>
+            </div>
+        );
+    });
 };
 
 export default App;
